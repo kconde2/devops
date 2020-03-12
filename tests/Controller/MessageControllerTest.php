8ddcb84b52a703f5508bc504dfeb8b0c670af9d6
Adam Malick Sow <adam.sow97@gmail.com>
2020-03-12 21:24:13 +0100

    one method added for each endpoint

---
+++ tests/Controller/MessageControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MessageControllerTest extends WebTestCase
+{
+    public function testGetMessage()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/messages');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testPostMessage()
+    {
+        $client = static::createClient();
+        $string = "Testing the POST endpoint";
+
+        $client->request('POST', '/messages', ['content' => $string]);
+
+        $responseData = json_decode($client->getResponse()->getContent(), true);
+        
+        $this->assertEquals(
+            strtoupper($string),
+            $responseData
+        );
+    }
+}
