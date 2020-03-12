8da0d966479d2420d78264386dcccc2b5ea52ddc
Kaba CONDE <kabaconde15@gmail.com>
2020-03-12 10:04:08 +0100

    Add message fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Message;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,8 +10,11 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 10; $i++) {
+            $message = new Message();
+            $message->setContent('Content ' . $i);
+            $manager->persist($message);
+        }
 
         $manager->flush();
     }
