b4bc7fedbca1cc09a19bf4cc9c8db20512e63eb9
Alex TEA <alextea2@gmail.com>
2020-03-12 12:11:06 +0100

    post/get routes

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -2,18 +2,44 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Repository\MessageRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class MessageController extends AbstractController
 {
     /**
-     * @Route("/message", name="message")
+     * @Route("/message", name="get_messages", methods={"GET"})
      */
-    public function index()
+    public function getMessages(MessageRepository $messageRepository, SerializerInterface $serializer)
     {
-        return $this->render('message/index.html.twig', [
-            'controller_name' => 'MessageController',
-        ]);
+        $messages = $messageRepository->findAll();
+        $messages = $serializer->serialize($messages, 'json');
+        return new Response($messages);
     }
+
+    /**
+     * @Route("/message", name="post_message", methods={"POST"})
+     */
+    public function postMessage(Request $request, EntityManagerInterface $em)
+    {
+        $content = $request->request->get('content', '');
+        if (empty($content)) {
+            return new JsonResponse("Content can't be empty", 400);
+        }
+        $message = (new Message())->setContent($content);
+        $em->persist($message);
+        $em->flush();
+        return new JsonResponse('Message saved');
+
+
+    }
+
+
 }
